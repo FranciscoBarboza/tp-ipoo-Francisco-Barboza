0197856a6969f9251dca596563c9d91200a99544
FranciscoBarboza <francisco.barboza.tomas@gmail.com>
2022-04-03 23:33:29 -0300

    Update testViaje.php

--- testViaje.php
+++ testViaje.php
@@ -20,12 +20,30 @@ do {
         case 1:
             reiniciarObj($viaje1);
             //menu para la opcion 1) cargar informacion de un viaje
-            echo "ingrese codigo de viaje: ";
-            $codigoN=trim(fgets(STDIN));
-            echo "\ningrese destino del viaje: ";
-            $destinoN=strtoupper(trim(fgets(STDIN)));
-            echo "\ningrese cantidad maxima de pasajeros permitidos en el viaje: ";
-            $cantMaxPasajerosN=trim(fgets(STDIN));
+            //validacion para no dejar vacio el codigo de viaje
+            do {
+                echo "\ningrese codigo de viaje: ";
+                $codigoN=trim(fgets(STDIN));
+                if ($codigoN==null) {
+                    echo "ERROR: debe ingresar un codigo.\n";
+                }
+            } while ($codigoN==null);
+            //validacion para ingresar detino del viaje
+            do {
+                echo "\ningrese destino del viaje: ";
+                $destinoN=strtoupper(trim(fgets(STDIN))); 
+                if ($destinoN==null) {
+                    echo "ERROR: debe ingresar un destino\n";
+                }
+            } while ($destinoN==null);
+            do {
+                echo "\ningrese cantidad maxima de pasajeros permitidos en el viaje: ";
+                $cantMaxPasajerosN=trim(fgets(STDIN));
+                if ($cantMaxPasajerosN==null) {
+                    echo "ERROR: debe ingresar una cantidad maxima de pasajeros.\n";
+                }
+            } while ($cantMaxPasajerosN==null);
+            
             //seteo todo
             $viaje1->setCodigo($codigoN);
             $viaje1->setDestino($destinoN);
@@ -76,48 +94,48 @@ do {
                 echo "\nque quiere modificar?\n";
                 linea();
                 echo "\n1). codigo del viaje.";
-                echo "\n2). destido del viaje.";
+                echo "\n2). destino del viaje.";
                 echo "\n3). cantidad maxima de pasajeros (no puede ser menor a la actual).";
                 echo "\n4). modificar pasajeros.";
-                echo "\n5). terminar de modificar.";
+                echo "\n5). terminar de modificar.\n";
                 $respuesta=trim(fgets(STDIN));
                 // validacion por sino ingresa un numero entre 1 o 3 por eso el do while
-                if (!(is_int($respuesta)) && !($respuesta>=0) && !($respuesta<=4)) {
-                    echo "\nerror: ingrese un numero entre 1 y 4";
+                if (!(is_int($respuesta)) && !($respuesta>=0) && !($respuesta<=5)) {
+                    echo "\nerror: ingrese un numero entre 1 y 5";
                 }
             } while (!(is_int($respuesta)) && !($respuesta>=0) && !($respuesta<=5));
                 
-            if ($respuesta=1) {
+            if ($respuesta==1) {
                 //1)codigo de viaje
                 echo "\ncodigo de viaje actual: ". $viaje1->getCodigo();
                 echo "\ningrese el codigo nuevo a cambiar: ";
                 $viaje1->setCodigo(trim(fgets(STDIN)));
             }
-            elseif($respuesta=2){
+            elseif($respuesta==2){
                 //2) destino de viaje
                 echo "\ndestino de viaje actual: ". $viaje1->getDestino();
                 echo "\ningrese el destino a cambiar: ";
                 $viaje1->setDestino(trim(fgets(STDIN)));
             }
-            elseif ($respuesta=3) {
+            elseif ($respuesta==3) {
                 //3)cant maxima de pasajeros
                 // pongo un do while para que repita el proceso en caso de ingresar un numero menor a la cantidad actual de pasajeros
                 do {
                     echo "\nla cantidad maxima de pasajeros: ". $viaje1->getCantMaxPasajeros();
                     echo "\ncantidad de pasajeros actuales: ". count($viaje1->getPasajerosViaje());
-                    echo "\ningrese la nueva cantidad de pasajeros: ";
+                    echo "\ningrese la nueva cantidad de pasajeros maxima: ";
                     $respuestaN=trim(fgets(STDIN));
-                    $respuestaN=$respuestaN-1;
-                    if ( !(is_int($respuesta)) && !($respuestaN <= (count($viaje1->getPasajerosViaje())))) {
-                        echo "error: ingrese un numero no menor al actual";
+                    if ( !(is_int($respuestaN)) && ($respuestaN <= (count($viaje1->getPasajerosViaje())))) {
+                        echo "error: ingrese un numero no menor al actual.\n";
                     }
-                } while ( !(is_int($respuesta)) && !($respuestaN <= (count($viaje1->getPasajerosViaje()))));
+                } while ( !(is_int($respuestaN)) && $respuestaN <= (count($viaje1->getPasajerosViaje())));
+                $viaje1->setCantMaxPasajeros($respuesta);
             }
-            elseif ($respuesta=4) {
+            elseif ($respuesta==4) {
                 //4)modificar pasajeros
                 do {
                     linea();
-                    echo "MODIFICAR PASAJEROS";
+                    echo "MODIFICAR PASAJEROS\n";
                     linea();
                     echo "1) ingresar pasajero nuevo.\n";
                     echo "2) eliminar pasajero.\n";
@@ -137,7 +155,7 @@ do {
                     } 
                     else {
                         echo "\ncantidad actual de pasajeros: ". (count($viaje1->getPasajerosViaje()));
-                        echo "\ncantidad de pasajeros maximo: ". ($viaje1->getCantMaxPasajeros());
+                        echo "\ncantidad de pasajeros maximo: ". ($viaje1->getCantMaxPasajeros()). "\n";
                         linea();
                         echo "\ningrese el nombre del nuevo pasajero: ";
                         $nombreN=strtoupper(trim(fgets(STDIN)));
@@ -145,7 +163,11 @@ do {
                         $apellidoN= strtoupper(trim(fgets(STDIN)));
                         echo "\ningrese documento del nuevo pasajero: ";
                         $documentoN= strtoupper(trim(fgets(STDIN)));
-                        array_push($viaje1->getPasajerosViaje(), ['nombre'=>$nombreN, 'apellido'=>$apellidoN, 'documento'=>$documentoN]);  
+                        //pusheo al array
+                        $aux=$viaje1->getPasajerosViaje();
+                        array_push($aux, ['nombre'=>$nombreN, 'apellido'=>$apellidoN, 'documento'=>$documentoN]);  
+                        $viaje1->setPasajerosViaje($aux);
+
                     }
                 }
                 elseif($respuesta==2){
@@ -166,13 +188,16 @@ do {
                     echo "\npasajero n°: ". $pasajeroN;
                     echo "\nnombre: ". $viaje1->darNombrePasajero($pasajeroN);
                     echo "\napellido: ". $viaje1->darApellidoPasajero($pasajeroN);
-                    echo "\ndocumento: ". $viaje1->darNroDeDocPasajero($pasajeroN);
+                    echo "\ndocumento: ". $viaje1->darNroDeDocPasajero($pasajeroN). "\n";
                     linea();
                     echo "desea eliminar este pasajero?(si/no): ";
                     $respuesta=strtoupper(trim(fgets(STDIN)));
                     if ($respuesta== "SI") {
-                        $nuevoArray= array_splice($viaje1->getPasajerosViaje(), $pasajeroN, 1);
+
+                        $nuevoArray=$viaje1->getPasajerosViaje();
+                        array_splice($nuevoArray, ($pasajeroN-1), 1);
                         $viaje1->setPasajerosViaje($nuevoArray);
+
                         echo "\nnueva cantidad de pasajeros: ". count($viaje1->getPasajerosViaje());
                     }
                     elseif ($respuesta=="NO") {
@@ -224,7 +249,7 @@ do {
             if ($respuesta==1){
                 //1). mostrar un pasajero en especifico
                 do {
-                    echo "1). ingrese el numero de pasajero";
+                    echo "ingrese el numero de pasajero: ";
                 $nroPasajeroN=trim(fgets(STDIN));
                 if (($nroPasajeroN > (count($viaje1->getPasajerosViaje()))) && ($nroPasajeroN > $viaje1->getPasajerosViaje()) && !(is_int($nroPasajeroN))) {
                     echo "\nerror: el numero de pasajero no existe o fue ingresado incorrectamente.";
@@ -251,13 +276,10 @@ do {
                 else {
                     echo "Pasajeros actuales: ". count($viaje1->getPasajerosViaje());
                 }
-                
             }
             break;
         case 4:
             echo "\nFIN PROGRAMA.";
             break;
-
-
     }
 } while ($respMainMenu<>4);
